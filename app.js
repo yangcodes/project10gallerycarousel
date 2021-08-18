f6cc45d3a648c2d195adf1dfabdd0350275cbefd
Jenny Lin <misslinyang91@gmail.com>
2021-08-18 17:39:58 +1000

    added the previous button function

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ function setImgPosition(img, index) {
 }
 imgs.forEach(setImgPosition);
 
+//moveToImg function
+const moveToImg = function (list, currentImg, targetImg) {
+  list.style.transform = "translateX(-" + targetImg.style.left + ")";
+  currentImg.classList.remove("current-img");
+  targetImg.classList.add("current-img");
+};
+
 //when we click on the right button, move images to the left
 nextBtn.addEventListener("click", function () {
   const currentImg = list.querySelector(".current-img");
@@ -20,8 +27,24 @@ nextBtn.addEventListener("click", function () {
   const nextIndex = imgs.findIndex(function (img) {
     img === nextImg;
   });
+  moveToImg(list, currentImg, nextImg);
+});
 
-  list.style.transform = "translateX(-" + nextImg.style.left + ")";
+/*list.style.transform = "translateX(-" + nextImg.style.left + ")";
   currentImg.classList.remove("current-img");
   nextImg.classList.add("current-img");
+}); */
+
+//when we click on the left button, move images to the right
+prevBtn.addEventListener("click", function () {
+  const currentImg = list.querySelector(".current-img");
+  const prevImg = currentImg.previousElementSibling;
+  const prevIndex = imgs.findIndex(function (img) {
+    img === prevImg;
+  });
+  moveToImg(list, currentImg, prevImg);
 });
+/*list.style.transform = "translateX(-" + prevImg.style.left + ")";
+  currentImg.classList.remove("current-img");
+  prevImg.classList.add("current-img");
+}); */
